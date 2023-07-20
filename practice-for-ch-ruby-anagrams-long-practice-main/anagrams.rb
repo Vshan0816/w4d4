200cde28a8c6909914819fa219c7421e33c95879
Vincent <vshan0816@gmail.com>
2023-07-20 15:57:03 -0400

    Finish anagrams phase2

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -3,10 +3,23 @@
 # p b.permutation.to_a
 
 
-def anagram?(str1, str2)
- permutation1 = str1.chars.permutation.to_a 
-  zi = str2.chars
-  permutation1.include?(zi)
+# def anagram?(str1, str2) #O(n!)
+#  permutation1 = str1.chars.permutation.to_a #n!
+#   zi = str2.chars
+#   permutation1.include?(zi) #n
+# end
+
+
+def anagram?(str1, str2) # n^2
+  str1.each_char do |char| # n
+    index = str2.index(char)
+    if index != nil #n
+      str2[index] = "" 
+    else
+      return false
+    end
+  end
+  return true if str2.length == 0
 end
 
 p anagram?("gizmo", "sally")
--- practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
@@ -1,10 +1,10 @@
 
 require "byebug"
-# def my_min(arr) #n^2
-#  arr.each do |num| #n
-#   return num if arr.all?{|n| num <= n} #n
-#  end
-# end
+def my_min(arr) #n^2
+ arr.each do |num| #n
+  return num if arr.all?{|n| num <= n} #n
+ end
+end
 
 
 
@@ -22,15 +22,15 @@ end
 
 # p my_min(list)
 
-# def largest_contiguous_subsum(arr) #n^2 + n == n^2
-#   new_arr = []
-#   (0...arr.length).each do |i| #n
-#     (i...arr.length).each do |j| #n
-#       new_arr << substring = arr[i..j].sum 
-#     end
-#   end
-#   new_arr.max #n
-# end
+def largest_contiguous_subsum(arr) #n^2 + n == n^2
+  new_arr = []
+  (0...arr.length).each do |i| #n
+    (i...arr.length).each do |j| #n
+      new_arr << substring = arr[i..j].sum 
+    end
+  end
+  new_arr.mc #n
+end
 
 
 def largest_contiguous_subsum(arr) #n
@@ -50,3 +50,6 @@ end
 
 list = [2, 3, -6, 7,-6,7]
 p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list) # => -1 (from [-1])
