eef6d39184b21714c7871f830232cfa057c22158
Vincent <vshan0816@gmail.com>
2023-07-20 13:51:41 -0400

    Finish phase 1, continuous subsum

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
@@ -1,20 +1,36 @@
 
 require "byebug"
+# def my_min(arr) #n^2
+#  arr.each do |num| #n
+#   return num if arr.all?{|n| num <= n} #n
+#  end
+# end
+
+
+
 def my_min(arr)
-  current = 0
-  arr.each.with_index do |num1, idx1|
-    arr.each.with_index do |num2, idx2|
-      debugger
-      if num1 < num2 
-        current = num1 
-      else
-        current = num2
-      end
+  arr.inject do |acc, el| #n
+    if acc < el
+      acc
+    else
+      el
     end
   end
-  current
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+#   list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 
-p my_min(list)
+# p my_min(list)
+
+def largest_contiguous_subsum(arr) #n^2 + n == n^2
+  new_arr = []
+  (0...arr.length).each do |i| #n
+    (i...arr.length).each do |j| #n
+      new_arr << substring = arr[i..j].sum 
+    end
+  end
+  new_arr.max #n
+end
+
+list = [2, 3, -6,7, -6, 7]
+p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
