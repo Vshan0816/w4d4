69bd409632e866507366694cd07fa2ae1838386c
tanziti <tanzitian3@gmail.com>
2023-07-20 16:17:26 -0400

    anagrams_finished

--- practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagrams.rb
@@ -3,14 +3,14 @@
 # p b.permutation.to_a
 
 
-# def anagram?(str1, str2) #O(n!)
-#  permutation1 = str1.chars.permutation.to_a #n!
-#   zi = str2.chars
-#   permutation1.include?(zi) #n
-# end
+def first_anagram?(str1, str2) #O(n!)
+ permutation1 = str1.chars.permutation.to_a #n!
+  zi = str2.chars
+  permutation1.include?(zi) #n
+end
 
 
-def anagram?(str1, str2) # n^2
+def second_anagram?(str1, str2) # n^2
   str1.each_char do |char| # n
     index = str2.index(char)
     if index != nil #n
@@ -22,5 +22,26 @@ def anagram?(str1, str2) # n^2
   return true if str2.length == 0
 end
 
-p anagram?("gizmo", "sally")
-p anagram?("elvis", "lives") 
+
+
+def third_anagram?(str1, str2)#2n + 2(nlogn) = O(nlogn)
+  sort1 = str1.chars.sort # n + (nlogn)
+  sort2 = str2.chars.sort # n + (nlogn)
+  sort1 == sort2
+
+end
+
+
+def fourth_anagram?(str1, str2) #2n = O(n)
+  hash1 = Hash.new(0)
+  str1.each_char do |char| #n
+    hash1[char] += 1
+  end
+  str2.each_char do |char| #n
+    hash1[char] -= 1
+  end
+  hash1.values.all? {|val| val == 0} #n (for bonus)
+end
+
+p fourth_anagram?("gizmo", "sally")
+p fourth_anagram?("elvis", "lives") 
